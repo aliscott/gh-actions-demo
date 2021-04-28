2c260186018d6deba26c365b81ddd9a538398bdc
Alistair Scott <ali.scott@gmail.com>
2021-04-28 14:17:58 +0100

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,22 @@
+
 provider "aws" {
-  region                      = "us-east-1" # <<<<< Try changing this to eu-west-1 to compare the costs
+  region     = "${local.aws_region}"
+  profile    = "${local.aws_profile}"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.30"
+    }
+  }
 }
 
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
-  instance_type = "t3.micro"              # <<<<< Try changing this to m5.8xlarge to compare the costs
-
+  instance_type = "t3.micro"
   root_block_device {
     volume_size = 10
   }
-}
-
-resource "aws_lambda_function" "hello_world" {
-  function_name = "hello_world"
-  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
-  handler       = "exports.test"
-  runtime       = "nodejs12.x"
-  memory_size   = 1024
 }
